98c1c94761dd15964c7da15bd2c07c570a3f6e5b
Britone Mwasaru <bmwasaru@gmail.com>
2023-08-28 00:11:49 +0300

    PEP8

--- send_sms.py
+++ send_sms.py
@@ -13,13 +13,15 @@ sender_id = os.environ.get("at_sender_id")
 africastalking.initialize(username, api_key)
 sms = africastalking.SMS
 
-# Validate a phone number using phonenumbers
+
+# Validate a phone number using phone numbers
 def validate_phone_number(phone_number):
     try:
         parsed_number = phonenumbers.parse(phone_number, "KE")  # Assuming Kenyan numbers
         return phonenumbers.is_valid_number(parsed_number)
     except phonenumbers.NumberParseException:
         return False
+
 
 # Send SMS to the specified phone numbers
 def send_sms(phone_numbers, message):
@@ -30,6 +32,7 @@ def send_sms(phone_numbers, message):
         status_summary[phone_number] = status
         click.echo(f"SMS sent to {phone_number}: {status}")
     return status_summary
+
 
 @click.command()
 @click.argument('csv_file', type=click.Path(exists=True))
@@ -43,7 +46,7 @@ def main(csv_file, message):
             phone_number = row.get("phone_numbers")
             if validate_phone_number(phone_number):
                 phone_numbers.append(phone_number)
-    
+
     if not phone_numbers:
         click.echo("No valid phone numbers found in the CSV file.")
         return
@@ -53,6 +56,7 @@ def main(csv_file, message):
     click.echo("\nSummary of SMS statuses:")
     for phone_number, status in summary.items():
         click.echo(f"{phone_number}: {status}")
+
 
 if __name__ == "__main__":
     main()
